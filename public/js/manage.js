6fccab8cc0f8b6081270fcc92727c7eca4c02991
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2021-12-07 22:25:41 +0000

    Manage transaction first party

--- public/js/manage.js
+++ public/js/manage.js
@@ -1,7 +1,32 @@
 accountObj = JSON.parse(localStorage.getItem('account'));
+
 // generer les codes à partir de l'objet account
+for (let i = 0; i < accountObj.code.length; i++) {
+    let opt1 = document.createElement('option')
+    opt1.innerHTML = accountObj.code[i];
+    document.getElementById('select-code1').appendChild(opt1);
+}
 
+// modfier dynamiquement la valeur du label
+document.getElementById('label1').value = accountObj.label[accountObj.code.indexOf(document.getElementById("select-code1").value)];
+let codeInput1 = document.getElementById("select-code1");
+codeInput1.onchange = () => {
+    document.getElementById('label1').value = accountObj.label[accountObj.code.indexOf(document.getElementById("select-code1").value)];
+}
 
+// generer les codes à partir de l'objet account
+for (let i = 0; i < accountObj.code.length; i++) {
+    let opt2 = document.createElement('option')
+    opt2.innerHTML = accountObj.code[i];
+    document.getElementById('select-code2').appendChild(opt2);
+}
+
+// modfier dynamiquement la valeur du label
+document.getElementById('label2').value = accountObj.label[accountObj.code.indexOf(document.getElementById("select-code2").value)];
+let codeInput2 = document.getElementById("select-code2");
+codeInput2.onchange = () => {
+    document.getElementById('label2').value = accountObj.label[accountObj.code.indexOf(document.getElementById("select-code2").value)];
+}
 
 const local = JSON.parse(localStorage.getItem("transaction"));
 
@@ -43,10 +68,12 @@ if (local != null) {
 bouton.onclick = () => {
     var new_date = document.getElementById('date-trans').value;
     var new_trans_obj = document.getElementById('trans-obj').value;
-    var new_code1 = document.getElementById('code1').value;
+    var new_code1 = document.getElementById('select-code1').value;
+    var new_label1 = document.getElementById('label1').value;
     var new_debit1 = document.getElementById('debit1').value;
     var new_credit1 = document.getElementById('credit1').value;
-    var new_code2 = document.getElementById('code2').value;
+    var new_code2 = document.getElementById('select-code2').value;
+    var new_label2 = document.getElementById('label2').value;
     var new_debit2 = document.getElementById('debit2').value;
     var new_credit2 = document.getElementById('credit2').value;
 
@@ -55,9 +82,11 @@ bouton.onclick = () => {
             date: [],
             trans_obj: [],
             code1: [],
+            label1: [],
             debit1: [],
             credit1: [],
             code2: [],
+            label2: [],
             debit2: [],
             credit2: []
         }
@@ -66,13 +95,15 @@ bouton.onclick = () => {
 
     var old_transaction = JSON.parse(localStorage.getItem('transaction'));
     // s'assurer que les champs sont non vides avant de les stocker
-    if (new_date && new_trans_obj && new_code1 && new_debit1 && new_credit1 && new_code2 && new_debit2 && new_credit2) {
+    if (new_date) {
         old_transaction.date.push(new_date);
         old_transaction.trans_obj.push(new_trans_obj);
         old_transaction.code1.push(new_code1);
+        old_transaction.label1.push(new_label1);
         old_transaction.debit1.push(new_debit1);
         old_transaction.credit1.push(new_credit1);
         old_transaction.code2.push(new_code2);
+        old_transaction.label2.push(new_label2);
         old_transaction.debit2.push(new_debit2);
         old_transaction.credit2.push(new_credit2);
     }
